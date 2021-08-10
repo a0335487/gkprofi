da8c52665ea4bab4c2994ce7e49abc806631f33f
a0335487 <88699161+a0335487@users.noreply.github.com>
2021-08-10 12:27:29 +0700

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,32 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['email'];
+$image = $_POST['image']
+
+
+$mail->isSMTP();
+$mail->Host = 'smtp.mail.ru';
+$mail->SMTPAuth = true;
+$mail->Username = 'parkerari@mail.ru';
+$mail->Password = '#dty564hg1234';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom('Aristageck@gmail.com');
+$mail->addAddress('parkerari@mail.ru');
+
+
+$mail->isHTML(true);
+
+$mail->Subject = 'Заявка с сайта';
+$mail->Body    = '' .$name. 'Оставил заявку' .phone. .image;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+  echo 'Error';
+} else {
+  header('location: thanks-you.html');
+}
+
+?>
